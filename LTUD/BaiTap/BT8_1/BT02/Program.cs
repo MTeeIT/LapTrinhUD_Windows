f2ea431cbb36b85429787f7fb60bba87596e74f5
MTeeIT <thunguyen9099@gmail.com>
2020-01-08 16:30:54 +0700

    Bai tap buoi dau

---
+++ LTUD/BaiTap/BT8_1/BT02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a, b, c, Denta, X1, X2;
+
+            Console.WriteLine("Nhap so a = ");
+            a = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap so b = ");
+            b = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap so c = ");
+            c = Double.Parse(Console.ReadLine());
+           
+        }
+    }
+}
---
+++ LTUD/BaiTap/BT8_1/BT8_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double DiemToan, DiemLy, DiemHoa, DTB;
+
+            Console.WriteLine("Nhap Diem toan: ");
+            DiemToan = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nnhap diem ly: ");
+            DiemLy = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nNhap diem hoa: ");
+            DiemHoa = Double.Parse(Console.ReadLine());
+
+            DTB = (DiemToan + DiemLy + DiemHoa) / 3;
+
+            Console.WriteLine("Diem vua nhap:");
+            Console.WriteLine("Toan: {0}", DiemToan);
+            Console.WriteLine("Ly: {0}", DiemLy);
+            Console.WriteLine("Hoa: {0}", DiemHoa);
+            Console.WriteLine("DTB: {0}", DTB);
+
+
+            if (DTB <= 5)
+                {
+                Console.WriteLine("Trung Binh");
+                }
+
+            else if (DTB > 5 && DTB < 8.5)
+                {
+                    Console.WriteLine("Kha");
+                }
+            else
+                {
+                    Console.WriteLine("Gioi");
+                }
+
+           
+            Console.ReadLine();
+
+
+        }
+    }
+}
